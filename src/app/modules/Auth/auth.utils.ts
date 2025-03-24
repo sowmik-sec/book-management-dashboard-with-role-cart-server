25e67beea0cb6233407863b47d6ad44656349e6f
sowmik-sec <sowmik.sec@gmail.com>
2025-03-24 12:13:14 +0600

    convert days to second for jwt sign

--- src/app/modules/Auth/auth.utils.ts
+++ src/app/modules/Auth/auth.utils.ts
@@ -1,13 +1,13 @@
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 export const createToken = (
   data: Record<string, unknown>,
   secret: string,
   expiresIn: number
 ) => {
-  return jwt.sign(data, secret, { expiresIn: `${expiresIn}` });
+  return jwt.sign(data, secret, { expiresIn: `${expiresIn * 24 * 60 * 60}` });
 };
 
 export const verifyToken = (token: string, secret: string) => {
-  return jwt.verify(token, secret);
+  return jwt.verify(token, secret) as JwtPayload;
 };
